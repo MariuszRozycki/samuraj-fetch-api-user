e4beedb996013d3920af6ee774c154071d10c027
MariuszRozycki <mariansc83@icloud.com>
2021-08-04 14:56:47 +0200

    Add fetch-api-react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    text: 'Type date',
+    error: 'Error in state init',
+    inputValue: 'Input is empty'
+  }
+
+  handleDateChange = () => {
+    const inputValue = this.refs.number.value;
+
+    this.setState({
+      inputValue
+    })
+
+    fetch(`http://numbersapi.com/${inputValue}/year?json`)
+      .then(response => {
+        if (response.ok) {
+          return response
+        }
+        throw Error('Something in wrong.')
+      })
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          text: data.text
+        })
+      )
+      .catch(err => console.error(err))
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <input onChange={this.handleDateChange} type="text" ref='number' />
+        <p >In this year: {this.state.text}</p>
+      </>
     );
   }
 }
